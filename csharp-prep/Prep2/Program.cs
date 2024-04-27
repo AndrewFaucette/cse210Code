59c9a93373c42e4b52e2900267775476b3747a2d
AndrewFaucette <andrewrfaucette@gmail.com>
2024-04-26 20:36:49 -0600

    wrote grade code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string gradeInput = Console.ReadLine();
+        int realGrade = int.Parse(gradeInput);
+        if (realGrade >= 90) {
+            Console.WriteLine("You got an A and you passed!");
+        } else if (realGrade >= 80) {
+            Console.WriteLine("You got an B and you passed!");
+        } else if (realGrade >= 70) {
+            Console.WriteLine("You got an C and you passed!");
+        } else if (realGrade >= 60) {
+            Console.WriteLine("You got an D and you failed");
+        } else {
+            Console.WriteLine("You got an F and you failed");
+        } 
     }
 }
