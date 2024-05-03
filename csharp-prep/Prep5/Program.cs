0cc1f87a21e47df3a22dd1e50f7ea63ba43a6113
AndrewFaucette <andrewrfaucette@gmail.com>
2024-05-02 22:05:53 -0600

    wrote function practice program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class Program {
+    static void Main(string[] args){
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int square = squareNumber(num);
+        DisplayResult(square, name);
+    }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber(){
+        Console.Write("Please enter your number: ");
+        string number = Console.ReadLine();
+        int num = int.Parse(number);
+        return num;
+    }
+    static int squareNumber(int num){
+        return num * num;
+    }
+    static void DisplayResult(int square, string name){
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
