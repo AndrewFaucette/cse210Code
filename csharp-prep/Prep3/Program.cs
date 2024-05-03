5892b7419346128a56b3c49fa0f520135e5fdeaa
AndrewFaucette <andrewrfaucette@gmail.com>
2024-05-02 21:10:47 -0600

    wrote number game program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program {
+    static void Main(string[] args){
+        string play = "y";
+        while (play == "y"){
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            string guess = "";
+            int gu3ss = 0;
+            int guesses = 0;
+            do {
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                gu3ss = int.Parse(guess);
+                if (gu3ss > magicNumber){
+                    Console.WriteLine("Lower");
+                } else if (gu3ss < magicNumber){
+                    Console.WriteLine("Higher");
+                } else {
+                    Console.WriteLine("Correct!");
+                }
+                guesses++;
+            } while (gu3ss != magicNumber);
+            Console.WriteLine($"It took you {guesses} guesses");
+            Console.Write("Do you want to keep playing?(y/n) ");
+            play = Console.ReadLine();
+        }
     }
 }
