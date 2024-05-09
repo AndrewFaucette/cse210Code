773395a9b808c56a0c4bc2c5d06680eb7a3fbb50
AndrewFaucette <andrewrfaucette@gmail.com>
2024-05-09 16:59:15 -0600

    fixed syntax errors

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,21 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        job job1 = new job();
+        Job job1 = new Job();
         job1._jobTitle = "firmware Engineer";
         job1._company = "intel";
         job1._startYear = 2016;
         job1._endYear = 2024;
         //job1.DisplayInfo();
 
-        job job2 = new job();
+        Job job2 = new Job();
         job2._jobTitle = "Janitor";
         job2._company = "Walmart";
         job2._startYear = 2011;
         job2._endYear = 2020;
         //job2.DisplayInfo();
 
-        resume resume1 = new resume();
+        Resume resume1 = new Resume();
         resume1._name = "Dave";
         //resume1._jobs;
         resume1._jobs.Add(job1);
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,10 +1,10 @@
-public class resume {
+public class Resume {
     public string _name = "";
-    public List<job> _jobs = new List<job>();
+    public List<Job> _jobs = new List<Job>();
 
     public void displayResume(){
         Console.WriteLine($"Name: {_name}\nJobs:");
-        foreach (job j in _jobs){
+        foreach (Job j in _jobs){
             j.DisplayInfo();
         }
     }
