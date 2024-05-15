f5da7c9a63185318c51ef736b35ee77d55a50e59
AndrewFaucette <andrewrfaucette@gmail.com>
2024-05-15 12:24:09 -0600

    added menu method and wrote program class

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Globalization;
 
 public class Journal {
-    public List<string> _prompts = new List<string>();
+    public List<string> _prompts = new List<string>{"What was your favorite part of today?", "What was the worst part of your day?", "Have you done any good in the world today?", "What did you learn today?", "How much wood would a wood chuck chuck if a wood chuck could chuck wood?"};
     public string _journalName;
     public List<Entry> _entries = new List<Entry>();
+    
     public void displayJournal(){
         foreach(Entry e in _entries){
             e.DisplayEntry();
@@ -14,9 +14,17 @@ public class Journal {
         Entry e = new Entry();
         DateTime localDate = DateTime.Now;
         e._entryDate = localDate.ToString();
+
         //e._prompt = 
-        Console.WriteLine("Entry:");
+        Console.Write("Entry: ");
         e._entryText = Console.ReadLine();
         _entries.Add(e);
+    }
+    public String displayMenu(){
+        Console.WriteLine("Choose an option (enter a number 1-4)");
+        Console.WriteLine("1. Write a new entry\n2. Display the Journal\n3. Save the journal to a file");
+        Console.WriteLine("4. Load the journal from a file\n5. quit the program(will load to file)");
+        String choice = Console.ReadLine();
+        return choice;        
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,11 +1,30 @@
 using System;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Journal journal1 = new Journal();
-        journal1.writeNewEntry();
-        journal1.displayJournal();
+        /*journal1.writeNewEntry();
+        journal1.displayJournal();*/
+        String menuOption;
+        do {
+            menuOption = journal1.displayMenu();
+            if(menuOption == "1"){
+                journal1.writeNewEntry();
+            } else if (menuOption == "2"){
+                journal1.displayJournal();
+            } else if (menuOption == "3"){
+                //save journal
+            } else if (menuOption == "4"){
+                //load journal
+            } else if (menuOption == "5"){
+                //load journal and exit
+            } else {
+                Console.WriteLine("That was not a correct input");
+            }
+        } while (menuOption != "5");
     }
 }
