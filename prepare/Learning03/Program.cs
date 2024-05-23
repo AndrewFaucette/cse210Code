308bd86833e4a15a0790cf45b65f9707e280197b
AndrewFaucette <andrewrfaucette@gmail.com>
2024-05-23 15:22:37 -0600

    coded the entire program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        Fraction two = new Fraction(6);
+        Console.WriteLine(two.GetFractionString());
+        Console.WriteLine(two.GetDecimalValue());
+        Fraction three = new Fraction(6,7);
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
     }
 }
