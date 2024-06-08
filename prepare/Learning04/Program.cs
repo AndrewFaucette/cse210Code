ae332f4c129bca9e0bc2bc0178183b96220c5be0
AndrewFaucette <andrewrfaucette@gmail.com>
2024-06-07 22:33:00 -0600

    coded the whole program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //MathAssignment mathAssignment = new MathAssignment("Samuel Bennet", "Multiplication", "Section 7.3", "Problems 8-19");
+        //Console.WriteLine(mathAssignment.getHomework());
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European history", "The causes of World War II");
+        Console.WriteLine(writingAssignment.getWritingInformation());
     }
 }
