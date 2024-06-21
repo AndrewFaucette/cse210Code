fbde16de4c8aa253378cbd0068049de583cdc455
AndrewFaucette <andrewrfaucette@gmail.com>
2024-06-21 12:16:13 -0600

    wrote the program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+class Program {
+    static void Main(string[] args) {
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("blue", 4);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("red", 3, 4);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("yellow", 8);
+        shapes.Add(circle);
+
+        foreach(Shape i in shapes){
+            Console.WriteLine($"color: {i.getColor()}");
+            Console.WriteLine($"area: {i.getArea()}");
+        }
     }
 }
